6d8a1001352abebcd1a34b2ae84b44124e8e345e
Vinicius Parizoto <vinicius@clientbase.com.br>
2022-11-30 11:48:13 -0300

    Manipulando requisições de Update e Delete

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,10 +1,12 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
   Post,
   Res,
 } from '@nestjs/common';
@@ -18,12 +20,22 @@ export class CoursesController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `Curso ${id}`;
+    return `Curso #${id}`;
   }
 
   @Post()
   @HttpCode(HttpStatus.NO_CONTENT)
   create(@Body() body) {
     return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `Atualização do Curso #${id}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Exclusão do Curso #${id}`;
   }
 }
