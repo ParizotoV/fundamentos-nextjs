55302ad22a60ee9ad8751266877b1ce06d35e043
Vinicius V Parizoto <vinicius@clientbase.com.br>
2023-03-31 12:20:33 -0300

    Exemplo de teste e2e - endpoint create course - parte 1

--- test/courses/courses.e2e-spec.ts
+++ test/courses/courses.e2e-spec.ts
@@ -1,28 +1,55 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { CoursesModule } from '../../src/courses/courses.module';
+import {CoursesModule} from '../../src/courses/courses.module'
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ValidationPipe } from '@nestjs/common';
+import { CreateCoursesDto } from '../../src/courses/dto/create-courses.dto';
+import { HttpStatus } from '@nestjs/common';
 
 describe('Courses: /courses', () => {
   let app: INestApplication;
 
+  const course: CreateCoursesDto = {
+    name: 'Nestjs com TypeORM',
+    description: 'Criando apis restful com nestjs',
+    tags: ['nestjs', 'typeorm', 'nodejs', 'typescript']
+  };
+
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [CoursesModule],
+      imports: [CoursesModule, TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      password: 'docker',
+      database: 'testdb',
+      autoLoadEntities: true,
+      synchronize: true,
+    })],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }));
+
     await app.init();
   });
 
   afterAll(async () => {
-    await app.close()
-  })
+    await app.close();
+  });
 
-  it('/ (GET)', () => {
+  it('Create POST /courses', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/courses')
+      .send(course)
+      .expect(HttpStatus.CREATED)
   });
 });
